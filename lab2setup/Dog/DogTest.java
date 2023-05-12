e0977aeb0bcef5f988ddc4196151dea3b3123125
Jay <3076190178@qq.com>
2023-05-12 21:24:49 +0800

    lab2

--- lab2setup/Dog/DogTest.java
+++ lab2setup/Dog/DogTest.java
@@ -1,7 +1,8 @@
 package Dog;
 
 import static org.junit.Assert.*;
-import org.junit.Test;
+import org.testng.annotations.Test;
+import org.junit.jupiter.api.Test;
 
 public class DogTest {    
     @Test
