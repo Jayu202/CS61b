652d5d2bf06ab1b08d43e7600f18daf16bd798f0
Jay <3076190178@qq.com>
2023-06-21 00:03:48 +0800

    first commit

--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -1,46 +1,35 @@
 package deque;
 
 public class LinkedListDeque<T> {
-    public class Node<T>{
-        Node<T> last;
-        T item;
-        Node<T> next;
-        public  Node(Node last,T i,Node next){
-            this.last = last;
-            item = i;
-            this.next = next;
-        }
-    }
-    int size = 0;
+    private int size = 0;
+    private final Node<T> sent = new Node<>(null,null,null);
 
-    Node sentF;
-    Node sentB;
-    public T LinkedListDeque(T x){
-        sentF = new Node(null,x,null);
-        sentB = sentF;
+    public LinkedListDeque(){
+        sent.next = sent;
+        sent.last = sent;
+        size = 0;
+    }
+    public LinkedListDeque(T x){
+        sent.last = new Node(sent,x,sent);
+        sent.next = sent.last;
         size = 1;
-        return (T) sentB.item;
     }
-
-//    public T getrecursive(int index){
-//    //递归的get
-//
-//    }
 
     public void addFirst(T item){
-        sentF = new Node(null,item,sentF);
+        sent.next = new Node(sent,item,sent.next);
+        sent.next.next.last = sent.next;
         size++;
-
     }
 
     public void addLast(T item){
-        sentB = new Node(sentB,item,null);
+        sent.last = new Node(sent.last,item,sent);
+        sent.last.last.next = sent.last;
         size++;
 
     }
 
     public boolean isEmpty(){
-        if(sentB==sentF)
+        if(size == 0)
             return true;
         else return false;
     }
@@ -50,29 +39,34 @@ public class LinkedListDeque<T> {
     }
 
     public void printDeque(){
-        Node p =sentF;
-        while (p!=null){
+        Node p =sent.next;
+        while (p!=sent){
             System.out.println(p.item);
             p = p.next;
         }
     }
 
     public T removeFirst(){
-        T x = (T) sentF.item;
-        sentF = sentF.next;
-        sentF.last = null;
+        if(size == 0){return null;}
+        T x = (T) sent.next.item;
+        sent.next = sent.next.next;
+        sent.next.last = sent;
+        size--;
         return x;
     }
 
     public T removeLast(){
-        T x = (T) sentB.item;
-        sentB = sentB.last;
-        sentB.next =null;
+        if(size == 0){return null;}
+        T x = (T) sent.last.item;
+        sent.next = null;
+        sent.last = sent.last.last;
+        sent.last.next = sent;
+        size--;
         return x;
     }
 
     public T get(int index){
-        Node p = sentF;
+        Node p = sent;
         while (index>0){
             p = p.next;
             index--;
@@ -88,5 +82,21 @@ public class LinkedListDeque<T> {
 //
 //    }
 
+
+    //    public T getrecursive(int index){
+//    //递归的get
+//
+//    }
+
+    public class Node<T>{
+    Node<T> last;
+    T item;
+    Node<T> next;
+    public  Node(Node last,T i,Node next){
+        this.last = last;
+        item = i;
+        this.next = next;
+    }
+}
 
 }
